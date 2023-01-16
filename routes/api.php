f122cb6ea4a138214acd9efcc0ed796bf63ae5b7
allandereal <ahullan@gmail.com>
2023-01-16 14:23:22 +0300

    setup routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\ColumnController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('columns', ColumnController::class)->only(['index', 'store', 'destroy']);
+Route::resource('cards', CardController::class)->only(['index', 'store']);
+
+Route::post('cards/swap-positions', [CardController::class, 'swapPositions']);
