957ce301f896b366e1a72ab4805343f41e560992
allandereal <ahullan@gmail.com>
2023-01-16 14:18:29 +0300

    create API resources

--- app/Http/Resources/CardResource.php
+++ app/Http/Resources/CardResource.php
@@ -14,6 +14,14 @@ class CardResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'column_id' => $this->column_id,
+            'description' => $this->description,
+            'position' => $this->position,
+            'created_at' => $this->created_at,
+            'deleted_at' => $this->deleted_at
+        ];
     }
 }
