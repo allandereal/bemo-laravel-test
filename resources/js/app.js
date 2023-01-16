c47d36be085219e8b7a5085f9abe3733233317c4
allandereal <ahullan@gmail.com>
2023-01-16 14:26:44 +0300

    setup vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import Vue from 'vue'
+
+//Main pages
+import App from './views/app.vue'
+import Board from './views/board.vue'
+
+import VModal from 'vue-js-modal'
+Vue.use(VModal)
+
+const app = new Vue({
+    el: '#app',
+    components: { App, Board }
+});
