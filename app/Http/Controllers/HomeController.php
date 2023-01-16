869d51df2c333eae020622c93e6f95644e4b1f70
allandereal <ahullan@gmail.com>
2023-01-16 14:27:05 +0300

    create home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Response;
+use Spatie\DbDumper\Databases\Sqlite;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function databaseExport()
+    {
+        $filename = 'kanban-dump.sql';
+        Sqlite::create()
+            ->setDbName(database_path('database.sqlite'))
+            ->dumpToFile($filename);
+
+        return Response::download($filename);
+    }
+}
