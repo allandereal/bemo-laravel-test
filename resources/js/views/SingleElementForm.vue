3d2fafa1d95d6d8e64e1e4c8899ce76ff152c1c3
allandereal <ahullan@gmail.com>
2023-01-21 09:37:59 +0300

    remove redundant testing code

--- resources/js/views/SingleElementForm.vue
+++ resources/js/views/SingleElementForm.vue
@@ -1,26 +1,19 @@
 <template>
-    <form class="form-inline" @submit.prevent="$emit('on-submit', form)">
+    <form class="form-inline" @submit.prevent="$emit('on-submit')">
         <slot></slot>
         <div class="form-inline__buttons">
             <button class="form-inline__button-save"  type="submit">Save</button>
-            <button class="form-inline__button-cancel" @click="$emit('on-cancel', form)" >+</button>
+            <button class="form-inline__button-cancel" @click="$emit('on-cancel')" >+</button>
         </div>
     </form>
 </template>
 <script>
 export default {
     name: 'SingleElementForm',
-    props: ['jj'],
+    props: [],
     data() {
-        return {
-            form: {
-                email: this.user?.email || "",
-                name: this.user?.name || "",
-            },
-        };
+        return {}
     },
-    methods: {
-        hi(){console.log(1111)},
-    }
+    methods: {}
 }
 </script>
